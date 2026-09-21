b7dfa92a5869393d2bcf56a37e0ddd728cd8765b
Afla Shareen <aflasherin07@email.com>
2026-09-21 09:54:19 +0530

    day 3 added

---
+++ day 3/dict.py
@@ -0,0 +1,17 @@
+name = "Afla"
+print(name[0])
+print(name[-1])
+print(name[2])
+
+text = "Python Programming"
+change = text.split(" ")
+frst = change[0]
+print(frst)
+print(change[-1])
+print(frst[0:6])
+
+text = "Python"
+print(text[::-1])
+
+
+n = 43261596
